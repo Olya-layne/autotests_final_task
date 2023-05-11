88accbe99704c011f544f0617dcfcb290246b18e
Olga Tarasova <konnova.o.d@mail.ru>
2023-05-11 22:11:49 +0500

    add login page tests

--- test_main_page.py
+++ test_main_page.py
@@ -2,11 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from selenium.common.exceptions import NoSuchElementException
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
     page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
