56a0c9e7afd13459e81989c57ab5b0f7cb330554
Olga Tarasova <konnova.o.d@mail.ru>
2023-05-16 16:21:49 +0500

    add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".add-to-basket .btn")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages :nth-child(1) > .alertinner")
+    BASKET_MESSAGE = (By.XPATH, "//div/div/div/p")
+    BOOK_NAME = (By.TAG_NAME, "h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    
